d7ea99b3c85b716f69f50b77242ebd0abc79aa6a
Maksym Dybarskyi <maksym@smartmobilefactory.com>
2018-05-22 18:15:03 +0200

    Fix lint in correct way

--- smftools/src/internal/java/de/smf/smftools/appcontainer/drawer/DebugDrawerLayout.java
+++ smftools/src/internal/java/de/smf/smftools/appcontainer/drawer/DebugDrawerLayout.java
@@ -490,12 +490,12 @@ public class DebugDrawerLayout extends ViewGroup implements DrawerLayoutImpl {
    * @see #LOCK_MODE_LOCKED_CLOSED
    * @see #LOCK_MODE_LOCKED_OPEN
    */
+  @SuppressLint("WrongConstant")
   public void setDrawerLockMode(@LockMode int lockMode, View drawerView) {
     if (!isDrawerView(drawerView)) {
       throw new IllegalArgumentException("View " + drawerView + " is not a " +
           "drawer with appropriate layout_gravity");
     }
-    @SuppressLint("WrongConstant")
     final int gravity = ((LayoutParams) drawerView.getLayoutParams()).gravity;
     setDrawerLockMode(lockMode, gravity);
   }
