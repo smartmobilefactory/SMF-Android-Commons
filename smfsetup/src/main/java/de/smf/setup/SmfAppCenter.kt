8176ce39f999b63ace8f951d6d0e03e6c2b50810
uwezippel <uwe.zippel@smartmobilefactory.com>
2019-10-28 13:44:00 +0100

    setup appCenter for inAppUpdates and crashReporting

---
+++ smfsetup/src/main/java/de/smf/setup/SmfAppCenter.kt
@@ -0,0 +1,64 @@
+package de.smf.setup
+
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.crashes.Crashes
+import com.microsoft.appcenter.distribute.Distribute
+
+object SmfAppCenter {
+
+    private lateinit var distributionConfig: DistributionConfig
+
+    fun setup(
+        application: Application,
+        appId: String,
+        distributionConfig: DistributionConfig
+    ): Boolean {
+        if (appId.isEmpty()) {
+            return false
+        }
+        this.distributionConfig = distributionConfig
+
+        AppCenter.configure(application, appId)
+        if (!Crashes.isEnabled().get()) {
+            AppCenter.start(Crashes::class.java)
+        }
+
+        application.registerActivityLifecycleCallbacks(ServiceStartCheck())
+        return true
+    }
+
+    private fun startDistributionService() {
+        if (distributionConfig.inAppUpdates && !Distribute.isEnabled().get()) {
+            AppCenter.start(Distribute::class.java)
+            Distribute.setEnabledForDebuggableBuild(true)
+        }
+    }
+
+    data class DistributionConfig(
+        val isUpdateActivity: (activity: Activity) -> Boolean = { true },
+        val inAppUpdates: Boolean = false
+    )
+
+    internal class ServiceStartCheck() : Application.ActivityLifecycleCallbacks {
+        override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+            if (distributionConfig.isUpdateActivity(activity)) {
+                startDistributionService()
+            }
+        }
+
+        override fun onActivityStarted(activity: Activity) {}
+
+        override fun onActivityResumed(activity: Activity) {}
+
+        override fun onActivityPaused(activity: Activity) {}
+
+        override fun onActivityStopped(activity: Activity) {}
+
+        override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
+
+        override fun onActivityDestroyed(activity: Activity) {}
+    }
+}
